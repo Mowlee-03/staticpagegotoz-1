95bc1a30f52f71ef2bcd02faee82333ee930224e
Mowlee-03 <mowleemm@gmail.com>
2024-11-20 15:08:17 +0530

    script-adjusted

--- script.js
+++ script.js
@@ -3,26 +3,13 @@ function togglebtn() {
         let data=document.querySelector(".mobile-bar")
         data.classList.toggle('active')
 }
-function answershow(button) {
-    // Find the parent `.questions` div
-    let questionDiv = button.closest('.questions');
-    let answer = questionDiv.querySelector('.q-answer');
-    answer.classList.toggle('seeanswer');
-    
-    //change the button symbol
-    let head=questionDiv.querySelector(".q-head")
-    let changeSymbol = questionDiv.querySelector('.q-dropdown');
-    let buttonElement = questionDiv.querySelector('.drop-btn'); // Get the button element
-    if (answer.classList.contains('seeanswer')) {
-        // When the answer is visible, show an upward arrow or minus icon
-        changeSymbol.src = "../Assets/Symbol-2.png"; // Update with the correct symbol path
-        buttonElement.style.backgroundColor = "#2B70FA"; // Change the background color
-    } else {
-        // When the answer is hidden, show a downward arrow or plus icon
-        changeSymbol.src = "../Assets/Symbol-3.png"; // Update with the correct symbol path
-        buttonElement.style.backgroundColor = ""; // Reset to default background color
-    }
-}
+
+document.querySelectorAll('.questions').forEach(question => {
+  const questionHead = question.querySelector('.q-head');
+  questionHead.addEventListener('click', () => {
+      question.classList.toggle('seeanswer');
+  });
+});
 
 const mainMenu = document.querySelector('.sticky');
 window.addEventListener('scroll', () => {
@@ -49,3 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     });
   });
+
+
+
+  
