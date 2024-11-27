7da71138cb7bd79c6c38f73b2d566a234ddd073f
Mowlee-03 <mowleemm@gmail.com>
2024-11-27 10:15:16 +0530

    changes

--- script.js
+++ script.js
@@ -67,5 +67,9 @@ const container=document.querySelector('.company-short-details')
   })
 
 
-
+(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+    w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+    m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+})(window,document,'script','http://167.86.121.150:8080/mtc.js','mt');
+mt('send', 'pageview');
   
